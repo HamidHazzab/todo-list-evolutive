7be4ff7798e57b86690133439ca41d34a8af4eda
HamidHazzab <hamid.hazzab.dev@protonmail.com>
2026-02-17 14:38:44 +0100

    Add task deletion and fix completed toggle

--- script.js
+++ script.js
@@ -4,22 +4,39 @@ let task = document.querySelectorAll('.task')
 
 const taskList = document.getElementById('task-list')
 
-
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
-    console.log(taskInput.value)
-
-    newTask = document.createElement('li')
+    
+    // Création de la tâche
+    let newTask = document.createElement('li')
     newTask.classList.add('task')
-
     newTask.textContent = taskInput.value
+
+    // Création du bouton supprimer (local pour chaque tâche)
+    let taskDeleteButton = document.createElement('button')
+    taskDeleteButton.classList.add('task-delete')
+    taskDeleteButton.textContent = 'Supprimer'
+    taskDeleteButton.type = 'button'
+
+    // Ajout du bouton DANS la tâche
+    newTask.appendChild(taskDeleteButton)
+
+    // Ajout de la tâche à la liste
     taskList.appendChild(newTask)
 
-    taskInput.value = ''
-    
+    // Listener pour barrer la tâche
     newTask.addEventListener ('click', () => {
-        
             newTask.classList.toggle('completed')
     })
+    
+    // Listener pour supprimer la tâche
+    taskDeleteButton.addEventListener ('click', (evt) => {
+       evt.stopPropagation()
+       newTask.remove()
+    })
+    
+    // Réinitialisation de l'input
+    taskInput.value = ''
+
 } )
 
